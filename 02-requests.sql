799d1b7003471b40c495734366fae25aa9cb80a3
Iana Darii <76789410+ianadarii@users.noreply.github.com>
2021-01-03 23:44:28 +0300

    Add files via upload

---
+++ 02-requests.sql
@@ -0,0 +1,31 @@
+--Total Sales
+select
+count(*),
+sum(Sales)
+from orders o 
+
+--Total Profit
+select
+count(*),
+sum(Profit)
+from orders o 
+
+--Profit per Order
+
+--Sales per Customer
+
+--Avg. Discount
+
+--Monthly Sales by Segment
+
+--Monthly Sales by Product Category
+
+--Sales by Product Category over time (Продажи по категориям)
+
+--Sales and Profit by Customer
+
+--Customer Ranking
+
+--Sales per region
+
+
